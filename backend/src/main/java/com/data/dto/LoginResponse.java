55310d6438cbe490b3eccc1fa93e5efcee4e93e7
Shramika <shramikapoojary62@gmail.com>
2026-01-23 20:06:21 +0530

    Add DTO layer

---
+++ backend/src/main/java/com/data/dto/LoginResponse.java
@@ -0,0 +1,14 @@
+package com.data.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class LoginResponse {
+	private String token;
+	private String email;
+	private String role;
+
+
+}
