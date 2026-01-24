741d9e0fab0d5acd14f48277af3aa6c6d99cd1ec
Shramika <shramikapoojary62@gmail.com>
2026-01-24 19:51:32 +0530

    Update Security layer

--- backend/src/main/java/com/data/security/JwtUtil.java
+++ backend/src/main/java/com/data/security/JwtUtil.java
@@ -17,15 +17,7 @@ public class JwtUtil {
 		return Keys.hmacShaKeyFor(secret.getBytes());
 	}
 	
-//	public String generateToken(UserDetails userDetails) {
-//        return Jwts.builder()
-//                .setSubject(userDetails.getUsername()) // email
-//                .claim("roles", userDetails.getAuthorities())
-//                .setIssuedAt(new Date())
-//                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60)) // 1 hr
-//                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
-//                .compact();
-//    }
+
 	
 	 public String generateToken(UserDetails userDetails) {
 	        return Jwts.builder()
@@ -46,15 +38,7 @@ public class JwtUtil {
 	                .getSubject();
 	 }
 	 
-//    public String extractToken(String token) {
-//    	return Jwts.parserBuilder()
-//    			   .setSigningKey(getKey())
-//    			   .build()
-//    			   .parseClaimsJws(token)
-//    			   .getBody()
-//    			   .getSubject();
-//    }
-//    
+ 
 	 public boolean validateToken(String token, UserDetails userDetails) {
 	        String username = extractUsername(token);
 	        return username.equals(userDetails.getUsername())
