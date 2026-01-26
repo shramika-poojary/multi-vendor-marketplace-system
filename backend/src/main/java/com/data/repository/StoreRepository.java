446193d7212c1ecedfec0c5044ebd960b67ba92c
Shramika <shramikapoojary62@gmail.com>
2026-01-26 19:49:06 +0530

    Add Store Repository

--- backend/src/main/java/com/data/repository/StoreRepository.java
+++ backend/src/main/java/com/data/repository/StoreRepository.java
@@ -6,12 +6,15 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.data.model.Store;
+import com.data.model.User;
 
 public interface StoreRepository extends JpaRepository<Store,Integer> {
 	
 	Optional<Store> findByStoreId(Integer storeId);
 
-	List<Store> findByVendor_UserId(Integer userId); // one vendor can have multiple stores so we use list here 
+	List<Store> findByVendor_Email(String email); // one vendor can have multiple stores so we use list here 
+
+	List<Store> findByVendor_UserId(int userId);
 	
 
 }
