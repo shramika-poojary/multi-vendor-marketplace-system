eb378c2f00458acf27824481b54a8d1d31370152
Shramika <shramikapoojary62@gmail.com>
2026-01-28 17:21:58 +0530

    Update product repository

--- backend/src/main/java/com/data/repository/ProductRepository.java
+++ backend/src/main/java/com/data/repository/ProductRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.data.dto.ProductResponseDTO;
 import com.data.model.Product;
 
 public interface ProductRepository extends JpaRepository<Product,Integer> {
