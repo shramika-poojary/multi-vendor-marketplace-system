e7687b42538ff07668601722d7b101b0ec6c53f0
Shramika <shramikapoojary62@gmail.com>
2026-02-01 21:53:46 +0530

    Update Order Repository

--- backend/src/main/java/com/data/repository/OrderRepository.java
+++ backend/src/main/java/com/data/repository/OrderRepository.java
@@ -9,4 +9,5 @@ import com.data.model.Order;
 public interface OrderRepository extends JpaRepository<Order, Integer> {
 
 	List<Order> findByCustomerUserId(int UserId);
+	List<Order> findByCustomerUserIdOrderByOrderDateDesc(int UserId);
 }
