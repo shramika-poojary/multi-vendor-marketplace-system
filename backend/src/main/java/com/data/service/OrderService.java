acbf1e2afc976a4c448736b203e0a4aa042d848d
Shramika <shramikapoojary62@gmail.com>
2026-02-02 21:59:16 +0530

    Update order service

--- backend/src/main/java/com/data/service/OrderService.java
+++ backend/src/main/java/com/data/service/OrderService.java
@@ -2,13 +2,15 @@ package com.data.service;
 
 import java.util.List;
 
+import com.data.dto.OrderHistoryResponseDto;
+import com.data.dto.OrderResponseDto;
 import com.data.model.Order;
 
 public interface OrderService {
 
-	Order placeOrder(int customerId);
+	OrderResponseDto placeOrder(int customerId);
 	Order getOrderById(int orderId);
-	List<Order> getOrdersByUser(int cutomerId);
+	List<OrderHistoryResponseDto> getOrdersByUser(int cutomerId);
 	boolean updateOrderStatus(int orderId,String status);
 	Order cancelOrder(int orderId);
 	
