d9d93f4c2f52e83d9d3effc4351dbcfbbd4703c1
Shramika <shramikapoojary62@gmail.com>
2026-02-12 17:23:03 +0530

    Add Web Config

---
+++ backend/src/main/java/com/data/config/WebConfig.java
@@ -0,0 +1,19 @@
+package com.data.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer{
+
+	 @Override
+	    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+	        
+	        registry.addResourceHandler("/images/**")
+//	                .addResourceLocations("\"C:\\multi-vendor-marketplace-system\\backend\\uploads\\stores\"");
+	        .addResourceLocations(
+	                "file:C:/multi-vendor-marketplace-system/backend/uploads/stores/"
+	            );
+	    }
+}
