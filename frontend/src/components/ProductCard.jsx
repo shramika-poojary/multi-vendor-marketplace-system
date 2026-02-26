c4cf05f5a1c23a7d615b0cf544141b77ca9b84a7
Shramika <shramikapoojary62@gmail.com>
2026-02-26 16:28:04 +0530

    Update Product Card

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -3,14 +3,14 @@ import { useNavigate } from "react-router-dom";
 
 function ProductCard({ product }) {
   const navigate = useNavigate();
-  const handleAddToCart=()=>{
+  const handleAddToCart = () => {
     addToCart({
-      productId:product.productId,
-      quantity:1
-    }).then(()=>{
+      productId: product.productId,
+      quantity: 1
+    }).then(() => {
       alert("Item Added to cart");
       navigate("/cart");
-    }).catch(err=>{
+    }).catch(err => {
       console.error(err);
       alert("Failed to add to cart");
     });
@@ -19,23 +19,28 @@ function ProductCard({ product }) {
   return (
     <div className="col-md-3 mb-4">
       <div className="card h-100 shadow-sm">
-        <div className="bg-light" style={{ height: "150px" }}>
+        <div className="bg-light d-flex align-items-center justify-content-center">
 
           <img
-  src={
-     product.imageURL
-      ? `http://localhost:8080${product.imageURL}`
-      : "https://via.placeholder.com/300x180?text=Product+imageURL"
-  }
-  alt={product.productName}
-  className="store-image"
-/>
+            src={
+              product.imageURL
+                ? `http://localhost:8080${product.imageURL}`
+                : "https://via.placeholder.com/300x180?text=Product+imageURL"
+            }
+            alt={product.productName}
+            className="store-image"
+            style={{
+              maxHeight: "100%",
+              objectFit: "cover",
+              width: "50%"
+            }}
+          />
         </div>
 
         <div className="card-body text-center">
           <h4>{product.productName}</h4>
-           <h6 className="text-muted">{product.productDescription}</h6>
-          <p>₹{product.price}</p>
+          <h6 className="text-muted">{product.productDescription}</h6>
+          <p>Price: ₹{product.price}</p>
 
           <button className="btn btn-sm btn-success" onClick={handleAddToCart}>
             Add to Cart
